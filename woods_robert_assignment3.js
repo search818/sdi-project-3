cd662e4f223cc6e979c21684b06f888a693a7801
Robert Woods <search818@fullsail.edu>
2012-12-01 14:21:02 -0500

    first commit

--- woods_robert_assignment3.js
+++ woods_robert_assignment3.js
@@ -1,1 +1,31 @@
 alert("JavaScript works!");
+
+
+
+var rsvp = function (name,age,gender) {
+  // Private
+  var donations = [];
+  var donationContent = function (item) {
+	if (item !== "cash") {
+	  donations.push(item);
+	} else {
+	  console.log("No cash donations allowed!")
+	}
+  };
+  var getName = function () { return name; };
+  return {
+  
+	"name": getName,
+	"age": getAge,
+	"gender": getGender,
+	"donationContent": donationContent
+	
+  };
+};
+ //rsvp names
+ var Tiffany = rsvp("Tiffany",36,"Female");
+ var Chuck   = rsvp("Chuck",42,"Male");
+ var Will    = rsvp("Will" ,39,"Male");
+ var Lisa    = rsvp("Lisa" ,48,"Female");
+  
+
