827c8043b150120182bd12e93c21a880d69c8194
Robert Woods <search818@fullsail.edu>
2012-12-02 01:18:52 -0500

    seconed commit

--- woods_robert_assignment3.js
+++ woods_robert_assignment3.js
@@ -1,31 +1,88 @@
 alert("JavaScript works!");
+//Robert Woods
+//
+//sdi 1211
+//Project 3 rsvp charity party
 
+
+var rsvpList= [];
 
 
 var rsvp = function (name,age,gender) {
-  // Private
   var donations = [];
-  var donationContent = function (item) {
-	if (item !== "cash") {
+  
+	
+	//functions
+	var addDonation = function (item) {
+	 for (var i =0; i < item.length; i++) {
+	  if (gender === "Female") {
+	    console.log(name + " has added " + item[i] + " to her donation.");
+	  } else {
+	    console.log(name + " has added " + item[i] + " to his donation.");
+	  } 
 	  donations.push(item);
-	} else {
-	  console.log("No cash donations allowed!")
+	
 	}
   };
-  var getName = function () { return name; };
-  return {
   
-	"name": getName,
+  var showDonation = function () {
+    for ( var i = 0; i < donationAmount.length; i++) {
+      console.log(name + " brought " + donations[i] + " for the charity party.");
+    };
+    return donation;
+  };
+  
+  var getAge = function () {
+    if(age < 18) {
+      console.log(name + " must be with an adult at the charity party")
+    } else {
+      console.log(name + " is an adult ")
+    };
+    return age;
+  };
+  
+  var getGender = function () {
+    return gender;
+  };
+  
+  return { 
+    "name": getName,
 	"age": getAge,
 	"gender": getGender,
-	"donationContent": donationContent
+	"addContents": addDonation,
+	"showContents": showDonation
 	
   };
 };
  //rsvp names
- var Tiffany = rsvp("Tiffany",36,"Female");
- var Chuck   = rsvp("Chuck",42,"Male");
- var Will    = rsvp("Will" ,39,"Male");
- var Lisa    = rsvp("Lisa" ,48,"Female");
+ var rsvp1 = rsvp("Tiffany",36,"Female");
+ rsvpList.push(rsvp1);
+ var rsvp2 = rsvp("Chuck",42,"Male");
+ rsvpList.push(rsvp2);
+ var rsvp3 = rsvp("Will",39,"Male");
+ rsvpList.push(rsvp3);
+ var rsvp4 = rsvp("Lisa",48,"Female");
+ rsvpList.push(rsvp4);
+ var rsvp5 = rsvp("Sara",17,"Female");
+ rsvpList.push(rsvp5);
+ 
+ rsvp1.addContents(["300 dollar check"]);
+ rsvp2.addContents(["350 dollar check"]);
+ rsvp3.addContents(["200 dollar check"]);
+ rsvp4.addContents(["400 dollar check"]);
+ rsvp4.addContents(["50 dollars"]);
+ 
+ console.log(rsvp1.name() + " is " + rsvp1.age());
+ console.log(rsvp2.name() + " is " + rsvp2.age());
+ console.log(rsvp3.name() + " is " + rsvp3.age());
+ console.log(rsvp4.name() + " is " + rsvp4.age());
+ console.log(rsvp5.name() + " is " + rsvp5.age());
+ 
+ 
+ 
+ 
+  
+ 
+ 
   
 
